21dc5cdaa5ee857c84a060a627dccdb20080ffa0
fardin379 <nrdsraz@gmail.com>
2024-07-29 22:46:05 +0530

    deploy bug fixed

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: {}, // Remove this if you don't need PostCSS
-  },
-  base: '/', // Set this if your app is hosted in a subdirectory, otherwise leave as '/'
-  server: {
-    host: "0.0.0.0",
-  },
-  build: {
-    outDir: 'dist', // Vercel will use this folder to serve your files
-  }
 })
